87103edcb98c4882b2208d05faf4dcc2011af1de
user <ojemehprogress@gmail.com>
2021-09-17 11:11:24 +0100

    initailized build

--- client/components/ecosystem.js
+++ client/components/ecosystem.js
@@ -14,7 +14,7 @@ const Ecosystem = () => {
             <div className={styles.sectionContainer}>
                 <div className="wow fadeInLeft" data-wow-duration="0.5s" data-wow-delay="0s">
                     <h2 className="sub-heading">WEquil coin! Our favour token used in all WEquil platforms</h2>
-                    <h2 className="sub-heading">Don't miss the chance to be part of the early birds</h2>
+                    <h2 className="sub-heading">Don&apos;t miss the chance to be part of the early birds</h2>
                 </div>
                 <div className="wow fadeInRight" data-wow-duration="0.5s" data-wow-delay="0s">
                     <img src="images/ecosystem.png" alt="WEquil ecosystem" className={styles.ecosystem} />
